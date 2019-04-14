95ab0b8dc0217e18c252ef3988973da351ccc90b
Ziyi ZHOU <ziyi.zhou@ibm.com>
2019-04-14 18:09:40 +0200

    contracts

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -173,7 +173,7 @@ function deploy_composer_rest_server {
     CF_APP_NAME=composer-rest-server-${BUSINESS_NETWORK_NAME}
     cf push \
         ${CF_APP_NAME} \
-        --docker-image ibmblockchain/composer-rest-server:${COMPOSER_VERSION} \
+        --docker-image sstone1/composer-rest-server:0.20.1 \
         -i 1 \
         -m 256M \
         --no-start \
